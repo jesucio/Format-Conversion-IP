2ab873e166358292f874cce6f0df0c62e62045eb
jesucio <this.is.jesus.ortiz@gmail.com>
2025-12-31 19:07:03 -0600

    Regular progress on I2S transmitter.

--- I2S_Transmitter/I2S_Transmitter.vhd
+++ I2S_Transmitter/I2S_Transmitter.vhd
@@ -6,18 +6,21 @@ use IEEE.numeric_std.all;
 
 
 ENTITY I2S_Transmitter is
+generic (
+	bit_depth : integer range 0 to 33 := 32;
+	sample_rate : integer range 40000 to 200000 := 48000
+		);
+
 port (
 	--inputs
-	bit_depth : in integer range 0 to 33 := 32;
-	sample_rate : in integer range 40000 to 200000 := 48000;
-
 	system_clock : in std_logic := '0';
 	data_CH1 : in signed((bit_depth - 1) downto 0);
 	data_CH2 : in signed((bit_depth - 1) downto 0);
-	data_latch : in std_logic := '0';
 	--outputs
+	data_latch : out std_logic := '0';
 	bclk : buffer std_logic := '0';
    wclk : buffer std_logic := '0';
+	mclk : buffer std_logic := '0';
 	data_out : out std_logic := '0'
 		);
 END I2S_Transmitter;
@@ -25,22 +28,22 @@ END I2S_Transmitter;
 ARCHITECTURE arch_I2S_Transmitter OF I2S_Transmitter IS
 -- constants                                                 
 -- signals                                                   
-signal mclk : std_logic := '0';
 --signal data_block_left, data_block_right : std_logic_vector((bit_depth - 1) downto 0);
-signal sync_flag : std_logic := '0';
 
 --test signals
 
 
 
 BEGIN
+
+mclk <= system_clock;
                                     
-gen_clock : process (system_clock,bclk,wclk) --generate I2S clocks
+gen_clock : process (system_clock,bclk,wclk,mclk) --generate I2S clocks
 
 	variable bclk_counter,wclk_counter : integer range 0 to 40000 := 0;
 	
 begin
-mclk <= system_clock;
+
 if falling_edge(mclk) then
 	if bclk_counter >= 3 then
 		bclk_counter := 0;
@@ -60,38 +63,31 @@ end if;
 end process gen_clock;
 
 
-send_data: process (bclk,wclk,data_latch,data_CH1,data_CH2)
+send_data: process (bclk,wclk,data_CH1,data_CH2)
 
 	variable prev_wclk : std_logic := '0';
 	variable data_to_send : std_logic_vector(((bit_depth * 2) - 1) downto 0);
-	variable bclk_counter : integer range 0 to 80 := 0;
+	variable bclk_counter : integer range 0 to 65 := 0;
 begin
 	
-	if falling_edge(wclk) then
-		sync_flag <= '1';
-	end if;
-	
-	if rising_edge(bclk) and sync_flag = '1' then
-		data_out <= data_to_send(((bit_depth * 2) - 1) - bclk_counter);
-		if bclk_counter >= ((bit_depth * 2) - 1) then
+	if rising_edge(bclk) then
+		if (prev_wclk /= wclk) and wclk = '1' then
+			data_to_send(((bit_depth) - 1) downto 0) := std_logic_vector(data_CH2);
+			data_to_send(((bit_depth * 2) - 1) downto bit_depth) := std_logic_vector(data_CH1);
 			bclk_counter := 0;
+			data_latch <= '1';
 		else
 			bclk_counter := bclk_counter + 1;
+			if bclk_counter >= 3 then
+				data_latch <= '0';
+			end if;
 		end if;
-		
+				
+		data_out <= data_to_send(((bit_depth * 2) - 1) - bclk_counter);
 		prev_wclk := wclk;
+	
 	end if;
 	
-	
-	if rising_edge(sync_flag) then
-		data_to_send(((bit_depth) - 1) downto 0) := std_logic_vector(data_CH2);
-		data_to_send(((bit_depth * 2) - 1) downto bit_depth) := std_logic_vector(data_CH1);
-		bclk_counter := 0;
-	end if;
-	
-	
-	
-
 end process send_data;
                       
 END arch_I2S_Transmitter;
